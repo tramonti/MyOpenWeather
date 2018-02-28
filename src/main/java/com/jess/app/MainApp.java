5473c53beaaad0d0e6ba07ad3a69da32cfe30860
Olesia Serban <OlesiaSerban@gmail.com>
2018-02-28 21:28:48 +0200

    refactored MainApp

--- src/main/java/com/jess/app/MainApp.java
+++ src/main/java/com/jess/app/MainApp.java
@@ -1,100 +1,106 @@
 package com.jess.app;
 
-
 import com.jess.app.view.IdEditDialogController;
 import com.jess.app.view.WeatherSceneController;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
-
-/**
- * Created by olesia on 07.05.16.
- */
 public class MainApp extends Application {
-
-
     private Stage primaryStage;
+    private Stage idEditStage;
+    private WeatherSceneController weatherSceneController;
+    private IdEditDialogController idEditDialogController;
 
     public MainApp() {
-
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-//        this.primaryStage.initStyle(StageStyle.UTILITY);
-        this.primaryStage.resizableProperty().setValue(Boolean.FALSE);
-        this.primaryStage.setTitle("My Open Weather");
-        this.primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
-        showWeatherOverview();
-
+        buildPrimaryStage();
+        this.primaryStage.show();
     }
 
-    public void showWeatherOverview() {
+    private void buildPrimaryStage() {
+        setUpPrimaryStage();
+        showWeatherOverview();
+    }
+
+    private void setUpPrimaryStage() {
+//        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setTitle("My Open Weather");
+        makeNotResizable(primaryStage);
+        primaryStage.setOnCloseRequest(event -> fullProgramExit());
+    }
+
+    private void makeNotResizable(Stage stage) {
+        stage.resizableProperty().setValue(Boolean.FALSE);
+    }
+
+    private void fullProgramExit() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    private void showWeatherOverview() {
         try {
-
-            FXMLLoader loader = new FXMLLoader();
-
-
-//            loader.setLocation(MainApp.class.getResource("view/weatherScene.fxml"));
-            loader.setLocation(MainApp.class.getClassLoader().getResource("view/weatherScene.fxml"));
-            AnchorPane weatherOverview = (AnchorPane) loader.load();
-
-            WeatherSceneController controller = loader.getController();
-            controller.setMainApp(this);
-
-            Scene scene = new Scene(weatherOverview);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+            FXMLLoader loader = getLoaderFromResource("view/weatherScene.fxml");
+            weatherSceneController = loader.getController();
+            weatherSceneController.setMainApp(this);
+            primaryStage.setScene(retrieveScene(loader));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public boolean showIdEditDialog(WeatherSceneController weatherSceneController){
+    private FXMLLoader getLoaderFromResource(String resourcePath) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getClassLoader().getResource(resourcePath));
+        loader.load();
+        return loader;
+    }
+
+    private Scene retrieveScene(FXMLLoader loader) throws IOException {
+        AnchorPane loaderPane = loader.getRoot();
+        return new Scene(loaderPane);
+    }
+
+    public boolean showIdEditDialog() {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getClassLoader().getResource("view/idChangeLayout.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
-
-            // Создаём диалоговое окно Stage.
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("City Change");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(primaryStage);
-            dialogStage.resizableProperty().setValue(false);
-            Scene scene = new Scene(page);
-            dialogStage.setScene(scene);
-
-            IdEditDialogController controller = loader.getController();
-            controller.setWeatherController(weatherSceneController);
-            controller.setDialogStage(dialogStage);
-            dialogStage.showAndWait();
-            return controller.isOkClicked();
-        }catch (IOException e){
+            setUpIdEditDialogStage();
+            setUpIdEditController();
+            idEditStage.showAndWait();
+            return idEditDialogController.isOkClicked();
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private void setUpIdEditDialogStage() {
+        idEditStage = new Stage();
+        idEditStage.setTitle("City Change");
+        idEditStage.initModality(Modality.WINDOW_MODAL);
+        idEditStage.initOwner(primaryStage);
+        makeNotResizable(idEditStage);
+    }
+
+    private void setUpIdEditController() throws IOException {
+        FXMLLoader loader = getLoaderFromResource("view/idChangeLayout.fxml");
+        idEditStage.setScene(retrieveScene(loader));
+        idEditDialogController = loader.getController();
+        idEditDialogController.setWeatherController(weatherSceneController);
+        idEditDialogController.setDialogStage(idEditStage);
     }
 }
